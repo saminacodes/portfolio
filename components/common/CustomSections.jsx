69a676bd0dab4a1b4f7f9b77227edcdc09c6c378
Kevin Canlas <kvncnlsdsgns@gmail.com>
2022-09-03 23:14:25 -0400

    Created branch

--- components/common/CustomSections.jsx
+++ components/common/CustomSections.jsx
@@ -2,12 +2,62 @@ import Image from "next/image";
 
 export const CustomSections = (props) => {
   return (
-    <section>
+    // <section>
+    //   <div className="wrapper">
+    //     <div className=" section-container">
+    //       <div className="section-text">
+    //         <h2 className={props.textColor}>{props.title}</h2>
+    //         <p className="section-content">{props.content}</p>
+    //         {props.link && (
+    //           <a
+    //             className="section-link"
+    //             target="_blank"
+    //             rel="noreferrer"
+    //             href={props.link}
+    //           >
+    //             {props.linkText}{" "}
+    //             <svg
+    //               xmlns="http://www.w3.org/2000/svg"
+    //               width="18"
+    //               height="16"
+    //               viewBox="0 0 18 16"
+    //               fill="none"
+    //             >
+    //               <path
+    //                 d="M1 8H17M10 1L17 8L10 15"
+    //                 stroke="white"
+    //                 strokeWidth="2"
+    //                 strokeLinecap="round"
+    //                 strokeLinejoin="round"
+    //               />
+    //             </svg>
+    //           </a>
+    //         )}
+    //       </div>
+    //       <div className="section-image">
+    //         {props.imageLink && (
+    //           <Image
+    //             src={props.imageLink}
+    //             layout="intrinsic"
+    //             width={480}
+    //             height={480}
+    //             objectFit="fit"
+    //           ></Image>
+    //         )}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+    <section className={`block hero`}>
       <div className="wrapper">
-        <div className=" section-container">
-          <div className="section-text">
-            <h2 className={props.textColor}>{props.title}</h2>
-            <p className="section-content">{props.content}</p>
+        <div className={`row ${props.row}`}>
+          <div className="col content-container">
+            <h2 className={`${props.textColor} ${props.titleType}`}>
+              {props.title}
+            </h2>
+            <p className="description">
+              {props.content}
+            </p>
             {props.link && (
               <a
                 className="section-link"
@@ -34,7 +84,7 @@ export const CustomSections = (props) => {
               </a>
             )}
           </div>
-          <div className="section-image">
+          <div className="col image-container">
             {props.imageLink && (
               <Image
                 src={props.imageLink}
--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,15 @@ export default function Home() {
     <main id="home" className="home">
       <CustomSections
         title="Hi, I'm Samina"
+        titleType='main-title'
         textColor="red-text"
         imageLink="/images/port-1.png"
         content="Developer Relations Engineer, Tech Educator, and Founder of Dark Mode Comics."
       ></CustomSections>
       <CustomSections
+        row="reverse"
         title="Developer Relations Engineer"
+        titleType='secondary-title'
         textColor="yellow-text"
         imageLink="/images/port-1.png"
         content="I am an education & partnerships focused developer relations engineer at thirdweb. I primarily work on educating users on how to use thirdweb or partner tools in their decentralized applications."
@@ -19,6 +22,7 @@ export default function Home() {
       ></CustomSections>
       <CustomSections
         title="Tech Educator"
+        titleType='secondary-title'
         textColor="blue-text"
         imageLink="/images/port-1.png"
         content="From teaching kids web development to teaching developers web3 development, I enjoy creating a wide variety of content easily accessible online."
@@ -26,7 +30,9 @@ export default function Home() {
         link="https://cookie-project.xyz"
       ></CustomSections>
       <CustomSections
+        row="reverse"
         title="Founder DarkModeComics"
+        titleType='secondary-title'
         textColor="green-text"
         imageLink="/images/port-1.png"
         content="What started as a little illustration turned into a series of comics to portray the day-to-day struggles of being a developer."
